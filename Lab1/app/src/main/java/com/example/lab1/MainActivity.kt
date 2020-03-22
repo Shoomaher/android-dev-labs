802b97fc287eadfb2f7df3b2d17557e149144d9b
Mi <mi.study17@gmail.com>
2020-03-22 05:10:57 +0200

    Add docs

--- Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -9,22 +9,58 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * Add 2 numbers
+     *
+     * @param a 1 argument
+     * @param b 2 argument
+     * @return result of addition
+     */
      fun add(a: Double, b: Double):Double {
         return a + b
     }
 
+    /**
+     * Divide 2 numbers
+     *
+     * @param a value which will be divided
+     * @param b value which gonna divide
+     * @return result of division - double of Infinity
+     */
     fun divide(a: Double, b: Double):Double {
         return a / b
     }
 
+    /**
+     * Multiply 2 numbers
+     *
+     * @param a 1 argument
+     * @param b 2 argument
+     * @return result of multiplication
+     */
     fun multiply(a: Double, b: Double):Double {
         return a * b
     }
 
+    /**
+     * Subsctract one number from another
+     *
+     * @param a value from which we substract
+     * @param b value which we substract from another
+     * @return result of subtraction
+     */
     fun substract(a: Double, b: Double):Double {
         return a - b
     }
 
+    /**
+     * Perform action and get result of calculation
+     *
+     * @param a raw string from 1 argument
+     * @param b raw string from 2 argument
+     * @param action function that gonna be executed for 2 numbers
+     * @return result of calculation or null if smth went wrong
+     */
     fun performCalculation(a: String, b: String, action: (Double, Double) -> Double): Double? {
         try {
             val firstArg = a.toDouble()
