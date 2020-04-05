e0ccf19657008dc2105ff140d0400ac1b755c7e4
Mi <mi.study17@gmail.com>
2020-04-05 14:29:54 +0300

    Setup CoronaVirusDetectedActivity layout

---
+++ Lab2/app/src/main/java/com/example/lab2/CoronaVirusDetectedActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lab2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoronaVirusDetectedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_corona_virus_detected)
+    }
+}
