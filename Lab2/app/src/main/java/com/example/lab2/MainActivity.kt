3c84d68b1722fb713a6a2fd25d7d30da12a86f45
Mi <mi.study17@gmail.com>
2020-04-05 14:58:44 +0300

    Implemented intent for opening CoronaVirusDetectedActivity

--- Lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.lab2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val coronaVirusDetected: Button = findViewById<Button>(R.id.coronaVirusDetectedBtn)
+
+        coronaVirusDetected.setOnClickListener {
+            val intent = Intent(this, CoronaVirusDetectedActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
